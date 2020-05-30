278fb7b41b0457a4d52913ae50b7f0724284902e
Stepan Popov <bimimot@yandex.ru>
2020-05-30 06:59:50 +0300

    Added babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
-const path = require('path');  // подключаем path к конфигу вебпак
+const path = require('path');
 
 module.exports = {
     entry: { main: './src/index.js' },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
+        },
+module: {
+    rules: [{ 
+        test: /\.js$/, 
+        use: { loader: "babel-loader" }, // весь JS обрабатывается пакетом babel-loader
+        exclude: /node_modules/ // исключает папку node_modules
+            }
+        ]
     }
-}
+};
